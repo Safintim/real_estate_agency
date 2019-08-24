3d50ee2bd7afac967825772aca5be20d0ae9bef1
Safin Timur <timurtlt96@mail.ru>
2019-08-24 12:44:19 +0300

    Enabled filter through new buildings on index page

--- property/admin.py
+++ property/admin.py
@@ -5,5 +5,7 @@ from .models import Flat
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
+    list_display = ['town', 'address', 'price', 'new_building', 'construction_year', ]
     search_fields = ['town', 'town_district', 'address', 'owner', ]
     readonly_fields = ['created_at', ]
+    list_editable = ['new_building', ]
