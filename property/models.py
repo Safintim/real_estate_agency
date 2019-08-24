7c2093283ef6ffeae9874bb042abb7fdcd43d80a
Safin Timur <timurtlt96@mail.ru>
2019-08-24 15:38:31 +0300

    Copied Flat.owners Owner

--- property/models.py
+++ property/models.py
@@ -45,7 +45,7 @@ class Owner(models.Model):
     name = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     owners_phone_pure = PhoneNumberField("Нормализированный номер владельца", region="RU")
-    flats = models.ManyToManyField(Flat, related_name='owner_flats', verbose_name="Квартиры в собственности")
+    flats = models.ManyToManyField(Flat, blank=True, related_name='owner_flats', verbose_name="Квартиры в собственности")
 
     def __str__(self):
         return self.name
