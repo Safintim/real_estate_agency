1abd3a3037b9e7409eeae856e5bd15c40ffbdd02
Safin Timur <timurtlt96@mail.ru>
2019-08-24 12:46:50 +0300

    Enabled filter to admin panel:new_building, rooms_number, has_balcony

--- property/admin.py
+++ property/admin.py
@@ -9,3 +9,4 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'town_district', 'address', 'owner', ]
     readonly_fields = ['created_at', ]
     list_editable = ['new_building', ]
+    list_filter = ['new_building', 'rooms_number', 'has_balcony', ]
